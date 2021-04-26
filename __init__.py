368b7b9ca0b56b2839668463c3f6459a37250dd1
aabbcco <foolyu_big_gun@qq.com>
2021-04-26 20:08:49 +0800

    we have a repeter

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 # import nonebot
 from nonebot import get_driver
-from nonebot import on_command
+from nonebot import on_command,on_keyword
 from nonebot.rule import to_me
 from nonebot.typing import T_State
 from nonebot.adapters.cqhttp import Bot,Event
 
 from .config import Config
 
-repeater = on_command('复读',aliases={"repeat"},rule=to_me())
+repeater =  on_command('rep',aliases={"repeat",'说'},rule=to_me(),priority=5)
 
 global_config = get_driver().config
 config = Config(**global_config.dict())
@@ -22,11 +22,11 @@ config = Config(**global_config.dict())
 
 @repeater.handle()
 async def setrepter(bot:Bot,event:Event,state:T_State):
-    args = str(event.get_message()).strip()
+    args = str(event.message).strip()
     if args:
-        state['repeat'] =args
+        state['repeat']=args
 
-@repeater.got('repeat',prompt='复读机')
+@repeater.got('repeat',prompt='复读模式开启')
 async def gotrepeat(bot:Bot,event:Event,state:T_State):
     strs = state['repeat']
     await repeater.finish(strs)
