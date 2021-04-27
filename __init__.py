426d04cc58f7238f1904fc9e995de8a5470f187e
aabbcco <foolyu_big_gun@qq.com>
2021-04-27 19:02:13 +0800

    minor changes for repeater

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 # import nonebot
 from nonebot import get_driver
-from nonebot import on_command,on_keyword
+from nonebot import on_command,on_keyword,on_startswith
 from nonebot.rule import to_me
 from nonebot.typing import T_State
 from nonebot.adapters.cqhttp import Bot,Event
 
 from .config import Config
 
-repeater =  on_command('rep',aliases={"repeat",'说'},rule=to_me(),priority=5)
+repeater = on_startswith('快说',rule=to_me(),priority=5)
 
 global_config = get_driver().config
 config = Config(**global_config.dict())
@@ -28,5 +28,5 @@ async def setrepter(bot:Bot,event:Event,state:T_State):
 
 @repeater.got('repeat',prompt='复读模式开启')
 async def gotrepeat(bot:Bot,event:Event,state:T_State):
-    strs = state['repeat']
+    strs = state['repeat'][2:]
     await repeater.finish(strs)
